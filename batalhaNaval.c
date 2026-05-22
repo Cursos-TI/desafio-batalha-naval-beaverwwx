0d9b03be436e83a1610454925c2c1458e83182c4
miguel <beaverxz123@gmail.com>
2026-05-22 16:50:19 +0000

    desafio mestre feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
+
     int main(){
 
     int tabuleiro[10][10];
-    int tamanhoNavio = 3;
-    int navioHorizontal[3] = {3, 3, 3};
-    int navioVertical[3] = {3, 3, 3};
-    int navioDiagonal1[3] = {3, 3, 3};
-    int navioDiagonal2[3] = {3, 3, 3};
+    
 
     for (int linha = 0; linha < 10; linha++) {
         for (int coluna = 0; coluna < 10; coluna++) {
@@ -15,39 +12,84 @@
         }
     }
 
-/// navios
-    int linhaH = 2;
-    int colunaH = 4;
-    int linhaV = 5;
-    int colunaV = 1;
-    int linhaD1 = 0;
-    int colunaD1 = 0;
-    int linhaD2 = 7;
-    int colunaD2 = 9;
+/// figuras
+    int cruz[5][5];
+    for (int cLinha = 0; cLinha < 5; cLinha++) {
+        for(int cColuna = 0; cColuna < 5; cColuna++) {
+        if (cColuna == 2 || cLinha == 2){
+           cruz[cLinha][cColuna] = 1;
+        } else {
+           cruz[cLinha][cColuna] = 0;
+        }
+    }
+    }
 
+    int origemLinha = 2;
+    int origemColuna = 2;
+    int linhaTabuleiro, colunaTabuleiro;
+
+    for (int cLinha = 0; cLinha < 5; cLinha++) {
+        for (int cColuna = 0; cColuna < 5; cColuna++) {
+            if (cruz[cLinha][cColuna] == 1 ) {
+            linhaTabuleiro = origemLinha + cLinha - 2;
+            colunaTabuleiro = origemColuna + cColuna - 2;
+            tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+            } 
+        }
+    }
+
+    int cone[5][5];
+    for (int dLinha = 0; dLinha < 5; dLinha++) {
+        for(int dColuna = 0; dColuna < 5; dColuna++) {
+            if(dLinha == 0 && dColuna == 2 || dLinha == 1 && dColuna >= 1 && dColuna <= 3 || dLinha == 2) {
+             cone[dLinha][dColuna] = 1; 
+            } else {
+             cone[dLinha][dColuna] = 0;
+            }
+        }
+    }
     
+    int origemLinha2 = 3;
+    int origemColuna2 = 7;
 
-     for(int i = 0; i < tamanhoNavio; i++) {
-        tabuleiro[linhaH][colunaH + i] = navioHorizontal[i];
+    for (int dLinha = 0; dLinha < 5; dLinha++) {
+        for (int dColuna = 0; dColuna < 5; dColuna++) {
+            if (cone[dLinha][dColuna] == 1 ) {
+            linhaTabuleiro = origemLinha2 + dLinha - 2;
+            colunaTabuleiro = origemColuna2 + dColuna - 2;
+            tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+            } 
+        }
+    }
 
-     }
+    int octaedro[5][5];
+    int origemLinha3 = 7;
+    int origemColuna3 = 4;
+    for(int oLinha = 0; oLinha < 5; oLinha++) {
+    for (int oColuna = 0; oColuna < 5; oColuna++) {
+        if (oLinha == 0 && oColuna == 2 || oLinha == 1 && oColuna >= 1 && oColuna <= 3 || oLinha == 2 || oLinha == 3 && oColuna >= 1 && oColuna <= 3 || oLinha == 4 && oColuna == 2) {
+            octaedro[oLinha][oColuna] = 1;
+        } else {
+            octaedro[oLinha][oColuna] = 0;
+        }
+       }
+    }
 
-     for (int v = 0; v < tamanhoNavio; v++) {
-        tabuleiro[linhaV + v][colunaV] = navioVertical[v];
-     }
-     
-     for (int d1 = 0; d1 < tamanhoNavio; d1++) {
-        tabuleiro[linhaD1 + d1][colunaD1 + d1] = navioDiagonal1[d1];
-     }
+    for (int oLinha = 0; oLinha < 5; oLinha++) {
+        for(int oColuna = 0; oColuna < 5; oColuna++) {
+            if(octaedro[oLinha][oColuna] == 1){
+                linhaTabuleiro = origemLinha3 + oLinha - 2;
+                colunaTabuleiro = origemColuna3 + oColuna - 2;
+                tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+            }
+        }
+    }
 
-    for (int d2 = 0; d2 < tamanhoNavio; d2++) {
-        tabuleiro[linhaD2 + d2][colunaD2 - d2] = navioDiagonal2[d2];
-     }
 
      printf("\nTABULEIRO BATALHA NAVAL\n");
-     for(int linha = 0; linha < 10; linha++){
-         for (int coluna = 0; coluna < 10; coluna++) {
-            printf(" %d", tabuleiro[linha][coluna]); 
+     for (int cLinha = 0; cLinha < 10; cLinha++) {
+        for(int cColuna = 0; cColuna < 10; cColuna++) {
+            printf(" %d", tabuleiro[cLinha][cColuna]); 
         }
         printf("\n");
          }
